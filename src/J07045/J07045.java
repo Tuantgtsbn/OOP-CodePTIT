d69cf47fa15ffed343e71e7ba3e54545a9125784
NguyenMinh Tuan <nguyenminhtuan28072003@gmail.com>
2023-10-04 18:42:14 +0700

    update

--- src/J07045/J07045.java
+++ src/J07045/J07045.java
@@ -1,10 +1,30 @@
 package J07045;
-
+import java.util.*;
+import java.io.*;
 public class J07045 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws IOException {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(new File("PHONG.in"));
+		int num=Integer.parseInt(sc.nextLine());
+		List<Phong> list=new ArrayList<Phong>();
+		while(num-->0) {
+			String[] arr=sc.nextLine().strip().split("\\s+");
+			list.add(new Phong(arr[0], arr[1], arr[2], arr[3]));
+			
+		}
+		Collections.sort(list, new Comparator<Phong>() {
 
+			@Override
+			public int compare(Phong o1, Phong o2) {
+				// TODO Auto-generated method stub
+				return o1.getName().compareTo(o2.getName());
+			}
+			
+		});
+		for (Phong i :list) {
+			System.out.println(i);
+		}
 	}
 
 }
--- src/J07045/Phong.java
+++ src/J07045/Phong.java
@@ -1,5 +1,36 @@
 package J07045;
 
 public class Phong {
+	private String kyhieu,name,dongia,phi;
 
+	public Phong(String kyhieu, String name, String dongia, String phi) {
+		super();
+		this.kyhieu = kyhieu;
+		this.name = name;
+		this.dongia = dongia;
+		this.phi = phi;
+	}
+
+	public String getKyhieu() {
+		return kyhieu;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDongia() {
+		return dongia;
+	}
+
+	public String getPhi() {
+		return phi;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.getKyhieu()+" "+this.getName()+" "+this.getDongia()+" "+this.getPhi();
+	}
+	
 }
